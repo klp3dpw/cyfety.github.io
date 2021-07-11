e83751229a449756c75f0252f53e347d9ee16f3b
klp3dpw <dianresky72@gmail.com>
2021-07-11 11:58:12 +0800

    update

--- application/modules/admin_home/controllers/Admin_home.php
+++ application/modules/admin_home/controllers/Admin_home.php
@@ -3,10 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin_home extends MY_Controller {
 	public function index(){
+		$this->load->view('admin_header');
+		$this->load->view('admin_navbar');
 		$this->load->view('admin_home');
+		$this->load->view('admin_footer');
 	}
 	
 	public function admin_artikel(){
 		$this->load->view('admin_artikel');
+	}
+
+	public function logout(){
+		session_destroy();
+		redirect('admin_login');
 	}
 }
