5e10d4a4021fe4ad4dfa3890723f1a10d27f910e
klp3dpw <dianresky72@gmail.com>
2021-07-11 19:42:45 +0800

    update 2

--- application/views/admin_navbar.php
+++ application/views/admin_navbar.php
@@ -22,7 +22,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="admin_home" class="brand-link text-center">
+      <a href="<?= base_url('admin_home') ?>" class="brand-link text-center">
         <!-- <img src="<?php echo base_url()?>assets_admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
           class="brand-image img-circle elevation-3" style="opacity: .8"> -->
         <span class="brand-text font-weight-light">ADMIN CYFETY</span>
@@ -46,13 +46,13 @@
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
             <li class="nav-item">
-              <a href="admin_artikel" class="nav-link">
+              <a href="<?= base_url('admin_artikel') ?>" class="nav-link">
                 <i class="nav-icon fas fa-file"></i>
                 <p>Artikel</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="admin_artikel" class="nav-link">
+              <a href="<?= base_url('admin_user') ?>" class="nav-link">
                 <i class="nav-icon fas fa-user"></i>
                 <p>User</p>
               </a>
