cc050e000c619f9cba6dca25698398c6199bf6c0
klp3dpw <85858395+klp3dpw@users.noreply.github.com>
2021-07-26 07:41:10 +0800

    Update admin_navbar.php

--- application/views/admin_navbar.php
+++ application/views/admin_navbar.php
@@ -58,7 +58,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="admin_artikel" class="nav-link">
+              <a href="admin_report" class="nav-link">
                 <i class="nav-icon fas fa-edit"></i>
                 <p>Report</p>
               </a>
